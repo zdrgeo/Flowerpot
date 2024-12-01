75a9ad23da5167d0b6018528922ba4a9bba03b33
zdrgeo <zdravko.georgiev@gmail.com>
2024-12-01 21:04:32 +0200

    Register DefaultAzureCredential

--- Device.Host/Program.cs
+++ Device.Host/Program.cs
@@ -1,7 +1,9 @@
 using Azure;
+using Azure.Identity;
 using Azure.Messaging.EventGrid;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Azure;
+using Microsoft.Azure.Devices.Client;
 using Device.Host;
 using Device.Services;
 // using Device.Services.Azure.IoTHub;
@@ -12,6 +14,8 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddAzureClients(
     clientBuilder =>
     {
+        clientBuilder.UseCredential(new DefaultAzureCredential());
+
         clientBuilder.AddEventGridPublisherClient(new Uri("<endpoint>"), new AzureKeyCredential("<access-key>"));
     }
 );
