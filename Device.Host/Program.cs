790882a11d1252e75a2a632c614413acbd4de0f4
zdrgeo <zdravko.georgiev@gmail.com>
2024-12-22 15:58:26 +0200

    Add Azure Relay based implementation of  Device service

--- Device.Host/Program.cs
+++ Device.Host/Program.cs
@@ -1,12 +1,11 @@
 using Azure;
 using Azure.Identity;
-using Azure.Messaging.EventGrid;
-using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Azure;
 using Device.Host;
 using Device.Services;
-using Device.Services.Azure.IoTHub;
+// using Device.Services.Azure.IoTHub;
 // using Device.Services.Azure.EventGrid;
+using Device.Services.Azure.Relay;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
@@ -18,13 +17,25 @@ builder.Services.AddAzureClients(
         clientBuilder.AddEventGridPublisherClient(new Uri("<endpoint>"), new AzureKeyCredential("<access-key>"));
     }
 );
+
+// Azure IoT Hub
+// builder.Services.AddSingleton<DesiredPropertiesChangeHandlerFactory>();
+// builder.Services.AddSingleton<CommandHandlerFactory>();
+// builder.Services.AddSingleton<PropertyServiceFactory>();
+// builder.Services.AddSingleton<TelemetryServiceFactory>();
+// // builder.Services.AddScoped<IDeviceService, DeviceService>();
+// builder.Services.AddScoped<IDeviceService, ProvisioningDeviceService>();
+// Azure IoT Hub
+
+// Azure Event Grid
 // builder.Services.AddScoped<ITelemetryService, TelemetryService>();
 // builder.Services.AddScoped<IDeviceService, DeviceService>();
-builder.Services.AddSingleton<DesiredPropertiesChangeHandlerFactory>();
-builder.Services.AddSingleton<CommandHandlerFactory>();
-builder.Services.AddSingleton<PropertyServiceFactory>();
-builder.Services.AddSingleton<TelemetryServiceFactory>();
-builder.Services.AddScoped<IDeviceService, ProvisioningDeviceService>();
+// Azure Event Grid
+
+// Azure Relay
+builder.Services.AddScoped<IDeviceService, DeviceService>();
+// Azure Relay
+
 builder.Services.AddHostedService<DeviceHostedService>();
 
 IHost host = builder.Build();
---
+++ Device.Services.Azure.Relay/DeviceService.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Net;
+using Microsoft.Azure.Relay;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Device.Services.Azure.Relay;
+
+public class DeviceServiceOptions {
+    public required string Address { get; set; }
+    public required string KeyName { get; set; }
+    public required string Key { get; set; }
+}
+
+public class DeviceService(IOptions<DeviceServiceOptions> options, ILogger<DeviceService> logger) : IDeviceService
+{
+    private readonly IOptions<DeviceServiceOptions> options = options ?? throw new ArgumentNullException(nameof(options));
+    private readonly ILogger<DeviceService> logger = logger;
+
+    const int Interval = 60_000;
+
+    public async Task RunAsync(CancellationToken cancellationToken)
+    {
+        TokenProvider tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(options.Value.KeyName, options.Value.Key);
+
+        HybridConnectionListener listener = new (new (options.Value.Address), tokenProvider);
+
+        listener.RequestHandler = HandleRequestAsync;
+
+        await listener.OpenAsync(cancellationToken);
+
+        if (logger.IsEnabled(LogLevel.Information))
+        {
+            logger.LogInformation("listening at: {time}", DateTimeOffset.Now);
+        }
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                logger.LogInformation("Reading sensors data at: {time}", DateTimeOffset.Now);
+            }
+
+            // Reading sensors data...
+
+            try { await Task.Delay(Interval, cancellationToken); } catch (TaskCanceledException) { }
+        }
+
+        await listener.CloseAsync(CancellationToken.None);
+    }
+
+    private async void HandleRequestAsync(RelayedHttpListenerContext context)
+    {
+        using StreamReader streamReader = new (context.Request.InputStream);
+
+        context.Response.StatusCode = HttpStatusCode.OK;
+        context.Response.StatusDescription = "OK";
+
+        using StreamWriter streamWriter = new (context.Response.OutputStream);
+
+        await streamWriter.WriteLineAsync("Hello from the device!");
+
+        await context.Response.CloseAsync();
+    }
+}
