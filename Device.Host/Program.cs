668d311dac36a700805f2e118cbb596dad6d8d97
zdrgeo <zdravko.georgiev@gmail.com>
2025-02-23 18:31:03 +0200

    Switch from mock to real humidity sensor

--- Device.Host/Program.cs
+++ Device.Host/Program.cs
@@ -7,15 +7,15 @@ HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.Configure<DeviceServiceOptions>(builder.Configuration.GetSection("DeviceService"));
 builder.Services.Configure<MockTemperatureSensorOptions>(builder.Configuration.GetSection("MockTemperatureSensor"));
-builder.Services.Configure<MockHumiditySensorOptions>(builder.Configuration.GetSection("MockHumiditySensor"));
+// builder.Services.Configure<MockHumiditySensorOptions>(builder.Configuration.GetSection("MockHumiditySensor"));
 builder.Services.Configure<MockIlluminanceSensorOptions>(builder.Configuration.GetSection("MockIlluminanceSensor"));
-// builder.Services.Configure<HumiditySensorOptions>(builder.Configuration.GetSection("HumiditySensor"));
+builder.Services.Configure<HumiditySensorOptions>(builder.Configuration.GetSection("HumiditySensor"));
 
 builder.Services.AddSystemd();
 builder.Services.AddSingleton<ITemperatureSensor, MockTemperatureSensor>();
-builder.Services.AddSingleton<IHumiditySensor, MockHumiditySensor>();
+// builder.Services.AddSingleton<IHumiditySensor, MockHumiditySensor>();
 builder.Services.AddSingleton<IIlluminanceSensor, MockIlluminanceSensor>();
-// builder.Services.AddSingleton<IHumiditySensor, HumiditySensor>();
+builder.Services.AddSingleton<IHumiditySensor, HumiditySensor>();
 builder.Services.AddSingleton<IDeviceService, DeviceService>();
 builder.Services.AddHostedService<DeviceHostedService>();
 
